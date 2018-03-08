2c6fc6911b8fe39adaccdba8b1c6cd6b5ba023c6
Taiwo <theycallmeflowz@yahoo.com>
2018-03-08 00:28:53 -0600

    init

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const exphbs = require('express-handlebars');
+// const exphbs = require('express-handlebars');
 const path = require('path');
 const nodemailer = require('nodemailer');
 
 const app = express();
 
 // View engine setup
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
+// app.engine('handlebars', exphbs());
+// app.set('view engine', 'handlebars');
 
 // Static folder
 app.use('/public', express.static(path.join(__dirname, 'public')));
