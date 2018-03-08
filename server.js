fb403e8b1233d4b1da4b8755711f0317b1e9b0d0
Taiwo Tejuosho <theycallmeflowz@yahoo.com>
2018-03-08 17:50:56 -0600

    img 1

--- server.js
+++ server.js
@@ -9,15 +9,17 @@ const PORT = process.env.PORT || 3000;
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
 // Body Parser Middleware
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views/index.html'));
+    res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
 app.post('/', (req, res) => {
-  const output = `
+    const output = `
     <h3>Contact Details</h3>
     <ul>  
       <li>Name: ${req.body.name}</li>
@@ -29,38 +31,38 @@ app.post('/', (req, res) => {
     <p>${req.body.message}</p>
   `;
 
-  // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    host: 'smtp.aol.com',
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-        user: "ttejuosho@aol.com", // generated ethereal user
-        pass: "kpmgwgci1A"  // generated ethereal password
-    }
-  });
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        host: 'smtp.aol.com',
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: "ttejuosho@aol.com", // generated ethereal user
+            pass: "kpmgwgci1A" // generated ethereal password
+        }
+    });
 
-  
-  // setup email data with unicode symbols
-  let mailOptions = {
-      from: '"Web Email" <ttejuosho@aol.com>', // sender address
-      to: "ttejuosho@aol.com", // list of receivers
-      subject: "New Email Message From Your Website", // Subject line
-      text: 'Hello world?', // plain text body
-      html: output // html body
-  };
 
-  // send mail with defined transport object
-  transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-          return console.log(error);
-      }
-      console.log('Message sent: %s', info.messageId);   
-      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: '"Web Email" <ttejuosho@aol.com>', // sender address
+        to: "ttejuosho@aol.com", // list of receivers
+        subject: "New Email Message From Your Website", // Subject line
+        text: 'Hello world?', // plain text body
+        html: output // html body
+    };
 
-      
-      res.sendFile(path.join(__dirname, 'views/index.html'));
-  });
-  });
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+
+        res.sendFile(path.join(__dirname, 'views/index.html'));
+    });
+});
 
 app.listen(PORT, () => console.log('Server started...'));
